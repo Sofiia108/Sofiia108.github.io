57ce764e56805a1037ba23dd7a2eabdd0cd1319f
Sofia <sofiia108@icloud.com>
2021-04-03 20:45:13 +0300

    add planet names to the page

--- Homework-13/Planets.js
+++ Homework-13/Planets.js
@@ -1,1 +1,12 @@
-console.log(localStorage.getItem("Planet-names"));
+const container = document.createElement('div');
+container.className = 'container';
+document.body.append(container);
+
+const planetsJSON = JSON.parse(localStorage.getItem("Planet-names"));
+
+planetsJSON.forEach(element => {
+    
+    let paragraph = document.createElement("P");                       
+    paragraph.innerHTML = `${Object.values(element)}`; 
+    container.appendChild(paragraph);           
+});
